7001a7fcd74e27a32aacb1b7d5ac246151247cca
itc-s22004 <s22004@std.it-college.ac.jp>
2023-02-15 15:25:08 +0000

    chapter7 ok

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,15 +1,31 @@
-// import {client} from 'lib/api'
+import { getPostBySlug } from 'lib/api'
+import Container from 'components/container'
 
-export default function Schedule() {
-    return <h1> 記事のタイトル </h1>
+export default function Schedule({
+    title,
+    publish,
+    content,
+    eyecatch,
+    categories,
+}) {
+    return (
+        <Container>
+            <h1>{title}</h1>
+        </Container>
+    )
 }
 
 export async function getStaticProps() {
-    console.log('処理1')
-    setTimeout(() => console.log('処理2'), 1000)
-    console.log('処理3')
+    const slug = 'schedule'
+    const post = await getPostBySlug(slug)
 
     return {
-        props: {},
+        props: {
+            title: post.title,
+            publish: post.publishDate,
+            content: post.content,
+            eyecatch: post.eyecatch,
+            categories: post.categories,
+        },
     }
 }
